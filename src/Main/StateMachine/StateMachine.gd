dfac46a101e6fd752bc5031a5b4b22bc9fa44d5d
denvermullets <devmullets@gmail.com>
2021-08-23 10:52:38 -0400

    add StateMachine logic

--- src/Main/StateMachine/StateMachine.gd
+++ src/Main/StateMachine/StateMachine.gd
@@ -1,3 +1,42 @@
 extends Node
-
 class_name StateMachine, "res://assets/icons/state_machine.svg"
+
+"""
+  Generic State Machine
+  Initializes states and delegates engine callbacks
+  (_physics_process, _unhandled_input) to the active state
+"""
+
+export var initial_state: = NodePath()
+
+onready var state: State = get_node(initial_state) setget set_state
+onready var _state_name: = state.name
+
+func _ready() -> void:
+  # owner will be the parent node, like 'Player' or 'Enemy'
+  # if the StateMachine is saved as its own scene, it will reference itself
+  # once the parent node emits 'ready' signal, the rest of the code will execute
+  yield(owner, "ready")
+func _init() -> void:
+  add_to_group("state_machine")
+  state.enter()
+
+func _unhandled_input(event: InputEvent) -> void:
+  state.unhandled_input(event)
+
+func _physics_process(delta: float) -> void:
+  state.physics_process(delta)
+
+func transition_to(target_state_path: String, msg: Dictionary = {}) -> void:
+  if not has_node(target_state_path):
+    return
+
+  var target_state: = get_node(target_state_path)
+
+  state.exit()
+  self.state = target_state
+  state.enter(msg)
+
+func set_state(value: State) -> void:
+  state = value
+  _state_name = state.name
