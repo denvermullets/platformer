947ed95eb76c544f9e0b7970f0dd38ecabdfad9c
denvermullets <devmullets@gmail.com>
2021-08-26 06:51:04 -0400

    add sprint

--- src/Player/States/Run.gd
+++ src/Player/States/Run.gd
@@ -1,10 +1,13 @@
 extends State
 
+export var max_speed_sprint := 900.0
+
 func unhandled_input(event: InputEvent) -> void:
   get_parent().unhandled_input(event)
 
 func physics_process(delta: float) -> void:
   var move := get_parent()
+  move.max_speed.x = max_speed_sprint if Input.is_action_pressed("sprint") else move.max_speed_default.x
 
   if owner.is_on_floor():
     if move.get_move_direction().x == 0.0:
